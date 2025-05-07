91626ace49e5eddeaf4a81341ed3139769d2be90
Alireza Kamru <alirezakamru@gmail.com>
2025-05-07 17:22:43 +0330

    add the string and void (no test fo sure the beta)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <windows.h>
 #include <ctime>
+#include <string>
 using namespace std;
 
+string GetTime(){
+    string info= "";
+    long long now = time(0);
+    return ctime(&now);
+};
+void ThrowAwayTheGabageInfo(){
+    string info = GetTime();
+    int index = info.find(":");
+
+    string hour;
+    hour = info[index-2];
+    hour +=info[index-1];
+
+    string minute;
+    minute = info[index+1];
+    minute +=info[index+2];
+
+    string second;
+    second = info[index+4];
+    second +=info[index+5];
+}
+
 int main(){
-
-
-
 
 return 0;
 }
